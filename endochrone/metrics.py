4fd2678da24500de5e6bcc491b7a79ddd66d49b3
nickwood <nickgwood@gmail.com>
2020-04-13 09:39:49 +0100

    Refactor Metrics into classes

--- endochrone/metrics.py
+++ endochrone/metrics.py
@@ -1,90 +1,95 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from functools import cached_property
 
 __author__ = "nickwood"
 __copyright__ = "nickwood"
 __license__ = "mit"
 
 
-def confusion_matrix(ytest, ypred):
-    labels = np.unique([ypred, ytest])
-    combinations = np.concatenate([ypred, ytest], axis=1)
-    combs, counts = np.unique(combinations, axis=0, return_counts=True)
-    return (labels, combs, counts)
+class Metrics:
+    def __init__(self, y_true, y_predicted):
+        self.y_true = y_true
+        self.y_pred = y_predicted
+
+    @cached_property
+    def confusion_matrix(self):
+        labels = np.unique([self.y_pred, self.y_true])
+        combinations = np.concatenate([self.y_pred, self.y_true], axis=1)
+        combs, counts = np.unique(combinations, axis=0, return_counts=True)
+        return (labels, combs, counts)
+
+    def print_confusion_matrix(self):
+        labels, comb, counts = self.confusion_matrix
+        N = len(labels)+1
+        matrix = [[0 for _ in range(N)] for _ in range(N)]
+        matrix[0][0] = r'pred\act'
+        for i in range(0, N-1):
+            matrix[0][i+1] = labels[i]
+            matrix[i+1][0] = labels[i]
+        for idx, (i, j) in enumerate(comb):
+            matrix[i+1][j+1] = counts[idx]
+
+        s = [[str(e) for e in row] for row in matrix]
+        lens = [max(map(len, col)) for col in zip(*s)]
+        fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+        table = [fmt.format(*row) for row in s]
+        print('\n'.join(table))
 
 
-def print_confusion_matrix(ytest, ypred):
-    labels, comb, counts = confusion_matrix(ytest, ypred)
-    N = len(labels)+1
-    matrix = [[0 for _ in range(N)] for _ in range(N)]
-    matrix[0][0] = r'pred\act'
-    for i in range(0, N-1):
-        matrix[0][i+1] = labels[i]
-        matrix[i+1][0] = labels[i]
-    for idx, (i, j) in enumerate(comb):
-        matrix[i+1][j+1] = counts[idx]
+class BinaryMetrics(Metrics):
+    @cached_property
+    def true_positive(self):
+        return np.sum(self.y_true & self.y_pred) / self.y_true.shape[0]
 
-    s = [[str(e) for e in row] for row in matrix]
-    lens = [max(map(len, col)) for col in zip(*s)]
-    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-    table = [fmt.format(*row) for row in s]
-    print('\n'.join(table))
+    @cached_property
+    def false_positive(self):
+        return np.sum(~self.y_true & self.y_pred) / self.y_true.shape[0]
 
+    @cached_property
+    def true_negative(self):
+        return np.sum(~self.y_true & ~self.y_pred) / self.y_true.shape[0]
 
-def true_positive(ytest, ypred):
-    return np.sum(ytest & ypred) / ytest.shape[0]
+    @cached_property
+    def false_negative(self):
+        return np.sum(self.y_true & ~self.y_pred) / self.y_true.shape[0]
 
+    @cached_property
+    def precision(self):
+        return self.true_positive / (self.true_positive + self.false_positive)
 
-def false_positive(ytest, ypred):
-    return np.sum(~ytest & ypred) / ytest.shape[0]
+    @cached_property
+    def recall(self):
+        return self.true_positive / (self.true_positive + self.false_negative)
 
-
-def true_negative(ytest, ypred):
-    return np.sum(~ytest & ~ypred) / ytest.shape[0]
-
-
-def false_negative(ytest, ypred):
-    return np.sum(ytest & ~ypred) / ytest.shape[0]
+    @cached_property
+    def f1_score(self):
+        return 2 * (self.precision * self.recall) /\
+                   (self.precision + self.recall)
 
 
-def precision(ytest, ypred):
-    return true_positive(ytest, ypred) /\
-           (true_positive(ytest, ypred) + false_positive(ytest, ypred))
+class MulticlassMetrics(Metrics):
+    @cached_property
+    def macro_precision(self):
+        labels, combs, counts = self.confusion_matrix
+        N = len(labels)
+        preds = [np.sum([counts[i] for i, (x, y) in enumerate(combs) if x == j])
+                 for j in range(N)]
+        tps = [counts[i] for i, (x, y) in enumerate(combs) if x == y]
+        precisions = np.array(tps) / np.array(preds)
+        return np.mean(precisions)
 
+    @cached_property
+    def macro_recall(self):
+        labels, combs, counts = self.confusion_matrix
+        N = len(labels)
+        acts = [np.sum([counts[i] for i, (x, y) in enumerate(combs) if y == j])
+                for j in range(N)]
+        tps = [counts[i] for i, (x, y) in enumerate(combs) if x == y]
+        recalls = np.array(tps) / np.array(acts)
+        return np.mean(recalls)
 
-def recall(ytest, ypred):
-    return true_positive(ytest, ypred) /\
-           (true_positive(ytest, ypred) + false_negative(ytest, ypred))
-
-
-def f1_score(ytest, ypred):
-    return 2 * (precision(ytest, ypred) * recall(ytest, ypred)) /\
-           (precision(ytest, ypred) + recall(ytest, ypred))
-
-
-def multiclass_precision(ytest, ypred):
-    "return macro precision"
-    labels, combs, counts = confusion_matrix(ytest, ypred)
-    N = len(labels)
-    preds = [np.sum([counts[i] for i, (x, y) in enumerate(combs) if x == j])
-             for j in range(N)]
-    tps = [counts[i] for i, (x, y) in enumerate(combs) if x == y]
-    precisions = np.array(tps) / np.array(preds)
-    return np.mean(precisions)
-
-
-def multiclass_recall(ytest, ypred):
-    "return macro recall"
-    labels, combs, counts = confusion_matrix(ytest, ypred)
-    N = len(labels)
-    acts = [np.sum([counts[i] for i, (x, y) in enumerate(combs) if y == j])
-            for j in range(N)]
-    tps = [counts[i] for i, (x, y) in enumerate(combs) if x == y]
-    recalls = np.array(tps) / np.array(acts)
-    return np.mean(recalls)
-
-
-def multiclass_f1_score(ytest, ypred):
-    prec = multiclass_precision(ytest, ypred)
-    recall = multiclass_recall(ytest, ypred)
-    return 2 * (prec * recall) / (prec + recall)
+    @cached_property
+    def macro_f1_score(self):
+        return 2 * (self.macro_precision * self.macro_recall) /\
+                   (self.macro_precision + self.macro_recall)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pytest
+from sklearn.metrics import precision_recall_fscore_support as prfs
 
-from endochrone import metrics
+from endochrone.metrics import BinaryMetrics
+from endochrone.metrics import MulticlassMetrics
 
 __author__ = "nickwood"
 __copyright__ = "nickwood"
@@ -12,12 +14,13 @@ __license__ = "mit"
 def test_small_confusion_matrix():
     ytest = np.transpose([[0, 1, 2, 2, 2, 2, 0, 2, 2, 0, 1, 1, 0, 1, 2, 0]])
     ypred = np.transpose([[0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 1]])
-    labels, comb, counts = metrics.confusion_matrix(ytest, ypred)
+    metrics = BinaryMetrics(ytest, ypred)
+    labels, comb, counts = metrics.confusion_matrix
     assert np.all(labels == np.array([0, 1, 2]))
     e_comb = np.array([[0, 0], [0, 2], [1, 0], [1, 1], [1, 2], [2, 1], [2, 2]])
     assert np.all(comb == e_comb)
     assert np.all(counts == np.array([4, 1, 1, 3, 2, 1, 4]))
-    metrics.print_confusion_matrix(ytest, ypred)
+    metrics.print_confusion_matrix()
 
 
 def test_large_confusion_matrix():
@@ -27,7 +30,8 @@ def test_large_confusion_matrix():
     ypred = np.transpose([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7,
                            8, 9, 1, 2, 3, 4, 5, 5, 7, 8, 9, 1, 2, 3, 4, 5, 6,
                            7, 8, 9, 1, 2, 3, 5, 6, 7, 8, 9, 1, 2, 3, 1, 3]])
-    labels, comb, counts = metrics.confusion_matrix(ytest, ypred)
+    metrics = BinaryMetrics(ytest, ypred)
+    labels, comb, counts = metrics.confusion_matrix
     assert len(labels) == 10
     assert comb.shape == (14, 2)
     e_counts = np.array([1, 7, 5, 1, 6, 1, 1, 3, 5, 1, 4, 5, 5, 5])
@@ -39,24 +43,26 @@ def test_binary_metrics():
                            False, False, False, True, False, False, True]])
     ypred = np.transpose([[False, True, False, False, True, False, True, True,
                            True, False, True, False, True, False, False]])
-    assert metrics.true_positive(ytest, ypred) == pytest.approx(2/15)
-    assert metrics.false_positive(ytest, ypred) == pytest.approx(5/15)
-    assert metrics.true_negative(ytest, ypred) == pytest.approx(4/15)
-    assert metrics.false_negative(ytest, ypred) == pytest.approx(4/15)
-    assert metrics.precision(ytest, ypred) == pytest.approx(2/7)
-    assert metrics.recall(ytest, ypred) == pytest.approx(2/6)
-    assert metrics.f1_score(ytest, ypred) == pytest.approx(4/13)
+    metrics = BinaryMetrics(ytest, ypred)
+    assert metrics.true_positive == pytest.approx(2/15)
+    assert metrics.false_positive == pytest.approx(5/15)
+    assert metrics.true_negative == pytest.approx(4/15)
+    assert metrics.false_negative == pytest.approx(4/15)
+    assert metrics.precision == pytest.approx(2/7)
+    assert metrics.recall == pytest.approx(2/6)
+    assert metrics.f1_score == pytest.approx(4/13)
 
 
 def test_multiclass_metrics():
     ytest = np.transpose([[0, 1, 2, 2, 2, 2, 0, 2, 2, 0, 1, 1, 0, 1, 2, 0]])
     ypred = np.transpose([[0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 1]])
-    assert metrics.multiclass_precision(ytest, ypred) == pytest.approx(7/10)
-    assert metrics.multiclass_recall(ytest, ypred) == pytest.approx(99/140)
-    assert metrics.multiclass_f1_score(ytest, ypred) == pytest.approx(693/985)
+    metrics = MulticlassMetrics(ytest, ypred)
+    assert metrics.macro_precision == pytest.approx(7/10)
+    assert metrics.macro_recall == pytest.approx(99/140)
+    assert metrics.macro_f1_score == pytest.approx(693/985)
 
 
-# test_small_confusion_matrix()
-# test_large_confusion_matrix()
-# test_binary_metrics()
-# test_multiclass_metrics()
+test_small_confusion_matrix()
+test_large_confusion_matrix()
+test_binary_metrics()
+test_multiclass_metrics()
