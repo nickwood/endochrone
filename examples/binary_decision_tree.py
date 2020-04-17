f5e189c1edb11577f2943fc2d4a038d496268df7
nickwood <nickgwood@gmail.com>
2020-04-17 11:20:41 +0100

    Simple binary tree example

---
+++ examples/binary_decision_tree.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+from endochrone.binary_decision_tree import BinaryDecisionTree
+
+__author__ = "nickwood"
+__copyright__ = "nickwood"
+__license__ = "mit"
+
+
+def plot_blobs():
+    x, y = make_blobs(n_samples=150, n_features=2, random_state=1234,
+                      cluster_std=1.5, centers=3)
+
+    bdt_test = BinaryDecisionTree(max_depth=3)
+    bdt_test.fit(x, y)
+    print(bdt_test)
+    plt.scatter(x[:, 0], x[:, 1], c=y)
+    plt.show()
+
+
+plot_blobs()
