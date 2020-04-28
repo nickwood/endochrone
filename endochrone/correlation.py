9be1512a3402b7172c78e9f016079f1c10e124b2
nickwood <nickgwood@gmail.com>
2020-04-28 11:41:32 +0100

    Refactor ACF and PACF  to classes

--- endochrone/correlation.py
+++ endochrone/correlation.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
+from functools import lru_cache
 from math import sqrt
 import numpy as np
-
-from endochrone.linear_regression import LinearRegression
 
 __author__ = "nickwood"
 __copyright__ = "nickwood"
@@ -18,27 +17,59 @@ def pearson(x, y):
     return covariance(x, y) / denominator
 
 
-def partial_correlation(x, y, others):
-    '''x and y are the two series we wish to calculate PACF for. Others is the
-    remaining variables whose residuals we wish to remove'''
-    x_other = LinearRegression(calculate_residuals=True)
-    x_other.fit(others, x)
-    x_res = x_other.residuals_
+class acf:
+    def __init__(self, x, lags=5):
+        self.x = x
+        self.mu = np.mean(x)
+        self.cent_x = x - self.mu
+        self.N = len(x)
+        self.lags = lags
 
-    y_other = LinearRegression(calculate_residuals=True)
-    y_other.fit(others, y)
-    y_res = y_other.residuals_
+    @lru_cache(None)
+    def auto_correlation(self, lag):
+        cov_lagged = np.sum(self.cent_x[lag:] * self.cent_x[:self.N-lag])
+        numerator = cov_lagged / (self.N - lag)
+        denominator = covariance(self.x, self.x) / self.N
+        return numerator / denominator
 
-    return pearson(x_res, y_res)
-
-
-def auto_correlation(x, lag):
-    mu = np.mean(x)
-    N = x.shape[0]
-    numerator = N * np.sum((x[(lag):]-mu) * (x[:N-(lag)]-mu))
-    denominator = (N - lag) * covariance(x, x)
-    return numerator / denominator
+    @property
+    def values(self):
+        return np.array([self.auto_correlation(lag)
+                         for lag in range(0, self.lags+1)])
 
 
-def acf(x, lags=3):
-    return np.array([auto_correlation(x, i) for i in range(0, lags+1)])
+class pacf(acf):
+    '''Using formulae from http://pecar-uk.com/Autocorrelations.pdf §5.5'''
+
+    @lru_cache(None)
+    def p_val_(self, i, j):
+        if i == j:
+            return self.p_val_with_same_index(i)
+        else:
+            return self.reduced_p_val_(i, j)
+
+    @lru_cache(None)
+    def p_val_with_same_index(self, i):
+        if i in [0, 1]:
+            return self.auto_correlation(i)
+        else:
+            n_sum = np.sum([self.p_val_(i-1, j) * self.auto_correlation(i-j)
+                            for j in range(1, i)])
+            numerator = self.auto_correlation(i) - n_sum
+            d_sum = np.sum([self.p_val_(i-1, j) * self.auto_correlation(j)
+                            for j in range(1, i)])
+            denominator = 1 - d_sum
+            return numerator / denominator
+
+    @lru_cache(None)
+    def reduced_p_val_(self, i, j):
+        return self.p_val_(i-1, j) - self.p_val_(i, i) * self.p_val_(i-1, i-j)
+
+    @property
+    def values(self):
+        return np.array([self.p_val_(lag, lag)
+                         for lag in range(0, self.lags+1)])
+
+    @property
+    def acf_values(self):
+        return super().values
--- tests/test_correlation.py
+++ tests/test_correlation.py
@@ -30,35 +30,40 @@ def test_pearson():
     assert np.abs(correlation.pearson(x, y_rand)) < 0.26
 
 
-def test_partial_correlation():
-    x = np.array([2, 4, 15, 20])
-    y = np.array([1, 2, 3, 4])
-    z = np.array([0, 0, 1, 1])
-    assert correlation.pearson(x, y) == pytest.approx(0.9695015519208121)
+def test_auto_correlation():
+    acf1 = correlation.acf(y_pos)
+    assert acf1.auto_correlation(2) == pytest.approx(0.95919592)
+    assert acf1.auto_correlation(7) == pytest.approx(0.85018502)
 
-    assert correlation.partial_correlation(x, y, z) == pytest.approx(0.9191450)
-    assert correlation.partial_correlation(x, x, z) == pytest.approx(1)
-    assert correlation.partial_correlation(x, z, y) == pytest.approx(0.9128709)
-    assert correlation.partial_correlation(y, z, x) == pytest.approx(-0.695222)
+    acf2 = correlation.acf(y_neg, lags=10)
+    assert acf2.auto_correlation(5) == pytest.approx(0.8949895)
+    assert acf2.auto_correlation(13) == pytest.approx(0.70617062)
+
+    acf3 = correlation.acf(y_per, lags=20)
+    assert acf3.auto_correlation(0) == pytest.approx(1)
+    assert acf3.auto_correlation(6) == pytest.approx(-0.27787815)
+    assert acf3.auto_correlation(16) == pytest.approx(0.27417115)
+    assert acf3.auto_correlation(18) == pytest.approx(0.78695581)
 
 
-def test_singular_auto_correlation():
-    assert correlation.auto_correlation(y_pos, 2) == pytest.approx(0.95919592)
-    assert correlation.auto_correlation(y_pos, 7) == pytest.approx(0.85018502)
-    assert correlation.auto_correlation(y_neg, 5) == pytest.approx(0.8949895)
-    assert correlation.auto_correlation(y_neg, 13) == pytest.approx(0.70617062)
-    assert correlation.auto_correlation(y_per, 0) == pytest.approx(1)
-    assert correlation.auto_correlation(y_per, 6) == pytest.approx(-0.27787815)
-    assert correlation.auto_correlation(y_per, 16) == pytest.approx(0.27417115)
-    assert correlation.auto_correlation(y_per, 18) == pytest.approx(0.78695581)
+acf_exp = np.array([1., 0.97979798, 0.95919592, 0.93819382, 0.91679168,
+                    0.8949895, 0.87278728, 0.85018502, 0.82718272, 0.80378038,
+                    0.779978])
 
 
 def test_acf():
-    acf_act = correlation.acf(x, lags=10)
-    acf_exp = np.array([1., 0.97979798, 0.95919592, 0.93819382, 0.91679168,
-                        0.8949895, 0.87278728, 0.85018502, 0.82718272,
-                        0.80378038, 0.779978])
+    acf_act = correlation.acf(x, lags=10).values
     assert np.all(acf_act == pytest.approx(acf_exp))
+
+
+def test_pacf():
+    pacf_cls = correlation.pacf(x, lags=10)
+    pacf_exp = np.array([1., 0.97979798, -0.0202061, -0.02042467, -0.0206562,
+                         -0.02090123, -0.02116036, -0.02143422, -0.02172351,
+                         -0.02202899, -0.02235147])
+
+    assert np.all(pacf_cls.values == pytest.approx(pacf_exp))
+    assert np.all(pacf_cls.acf_values == pytest.approx(acf_exp))
 
 
 ltr()
