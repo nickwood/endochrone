09c1c431ae8d2a5d95e8a357580c0f098d76e680
nickwood <nickgwood@gmail.com>
2020-05-10 12:00:35 +0100

    Approximate Jacobian

--- endochrone/optimisation/gradient_descent.py
+++ endochrone/optimisation/gradient_descent.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+from typing import Iterable
 
 __author__ = "nickwood"
 __copyright__ = "nickwood"
@@ -9,3 +11,25 @@ class GradientDescent:
     def __init__(self):
         raise NotImplementedError
 
+
+def approx_jacobian(functions: Iterable[callable], point: dict, delta=0.00001):
+    '''Calculates the approximate jacobian at a specified point. Note that we
+    take an iterable of functions so we can generate the Jacobian for
+    non-scalar functions
+    e.g:
+    def f(*, a, b):
+        return (np.abs(a - 1.5) + np.abs(b - 1.5))
+    point = {'a': 1, 'b': 1}
+    approx_jacobian([f], point) ~> np.array([[-1, -1]])'''
+
+    N = len(point.keys())
+    M = len(functions)
+    jac = np.zeros((M, N))
+
+    f_initial = np.array([functions[j](**point) for j in range(M)])
+
+    for i, k in enumerate(point.keys()):
+        delta_point = dict(point, **{k: point[k] + delta})
+        delta_f = np.array([functions[j](**delta_point) for j in range(M)])
+        jac[:, i] = (delta_f - f_initial) / delta
+    return jac
--- tests/optimisation/test_gradient_descent.py
+++ tests/optimisation/test_gradient_descent.py
@@ -1,13 +1,94 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import pytest
+
 from endochrone.utils import lazy_test_runner as ltr
+import endochrone.optimisation.gradient_descent as gd
+from endochrone.optimisation import GradientDescent
 
 __author__ = "nickwood"
 __copyright__ = "nickwood"
 __license__ = "mit"
 
 
-def test_nothing():
-    assert True
+def test_approx_jacobian_1x2():
+    def abs_function(*, a, b):
+        return (np.abs(a - 1.5) + np.abs(b - 1.5))
+
+    p0 = {'a': 1, 'b': 1}
+    jac0 = gd.approx_jacobian([abs_function], p0)
+    assert jac0.shape == (1, 2)
+    assert np.all(jac0[0] == pytest.approx(-1.))
+
+    p1 = {'a': 1, 'b': 2}
+    jac1 = gd.approx_jacobian([abs_function], p1)
+    assert jac1.shape == (1, 2)
+    assert jac1[0, 0] == pytest.approx(-1.)
+    assert jac1[0, 1] == pytest.approx(1.)
+
+    p2 = {'a': 2, 'b': 2}
+    jac2 = gd.approx_jacobian([abs_function], p2)
+    assert jac2.shape == (1, 2)
+    assert jac2[0, 0] == pytest.approx(1.)
+    assert jac2[0, 1] == pytest.approx(1.)
+
+
+def test_approx_jacobian_2x2():
+    def f1(*, x, y):
+        return (x**2 * y)
+
+    def f2(*, x, y):
+        return (5*x + np.sin(y))
+
+    funcs = [f1, f2]
+
+    p0 = {'x': 1, 'y': 1}
+    jac0 = gd.approx_jacobian(funcs, p0)
+    assert jac0.shape == (2, 2)
+    print(jac0)
+    assert jac0[0, 0] == pytest.approx(2., abs=0.0001)
+    assert jac0[0, 1] == pytest.approx(1., abs=0.0001)
+    assert jac0[1, 0] == pytest.approx(5., abs=0.0001)
+    assert jac0[1, 1] == pytest.approx(0.540302, abs=0.0001)
+
+
+def test_approx_jacobian_3x4():
+    # https://en.wikipedia.org/wiki/Jacobian_matrix_and_determinant#Example_4
+    def f1(*, x, y, z):
+        return x
+
+    def f2(*, x, y, z):
+        return 5 * z
+
+    def f3(*, x, y, z):
+        return 4 * y**2 - 2 * z
+
+    def f4(*, x, y, z):
+        return z * np.sin(x)
+
+    funcs = [f1, f2, f3, f4]
+
+    p0 = {'x': np.pi, 'y': 1, 'z': 2}
+    jac = gd.approx_jacobian(funcs, p0)
+    assert jac.shape == (4, 3)
+    exp = np.array([[1., 0., 0.],
+                    [0., 0., 5.],
+                    [0., 8., -2.],
+                    [-2., 0., 0.]])
+    assert np.all(jac == pytest.approx(exp, abs=0.0001))
+
+
+def test_gradient_descent():
+    def abs_function(*, a, b):
+        return (np.abs(a - 1.5) + np.abs(b - 1.5))
+
+    with pytest.raises(NotImplementedError):
+        gs_test = GradientDescent()
+        gs_test.fit(abs_function)
+
+    # assert gs_test.minimum == pytest.approx(0.0)
+    # assert gs_test.min_args['a'] == pytest.approx(1.5)
+    # assert gs_test.min_args['b'] == pytest.approx(1.5)
 
 
 ltr()
