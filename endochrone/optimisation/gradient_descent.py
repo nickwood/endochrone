03c036558a6eaf9f623d33f92418835b5ef5d1bf
nickwood <nickgwood@gmail.com>
2020-05-10 09:41:12 +0100

    Stubs to fix tests

---
+++ endochrone/optimisation/gradient_descent.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "nickwood"
+__copyright__ = "nickwood"
+__license__ = "mit"
+
+
+class GradientDescent:
+    def __init__(self):
+        raise NotImplementedError
+
---
+++ tests/optimisation/test_gradient_descent.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from endochrone.utils import lazy_test_runner as ltr
+
+__author__ = "nickwood"
+__copyright__ = "nickwood"
+__license__ = "mit"
+
+
+def test_nothing():
+    assert True
+
+
+ltr()
